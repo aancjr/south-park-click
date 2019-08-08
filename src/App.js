ed8082ecaf13bd57f1a0cfff202bb1b5efd4e1cc
aancjr <45945137+aancjr@users.noreply.github.com>
2019-08-07 20:15:45 -0400

    cards count in navbar with each click, shuffling after click, but only with first click. still need to limit count, show high score and have shuffle with each click

--- src/App.js
+++ src/App.js
@@ -7,20 +7,50 @@ import friends from "./friends.json";
 
 import './App.css';
 
+
+
 class App extends Component {
 
   state = {
-    friends
+    clicked: [],
+    friends,
+    count: 0
   };
 
+  southParkMemory = id => {
+    const character = this.state.friends.filter(friend => friend.id !== id);
+    console.log(character);
+    this.setState({ character, count: this.state.count + 1 });
+  }
+
+  shuffleCards = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * arr.length)
+      const temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
+    }
+    return arr
+  };
+
+  componentDidMount() {
+
+        this.shuffleCards(friends);
+
+}
+
+
   render() {
+    
     return (
       <Wrapper>
-        <Navbar />
+        <Navbar count={this.state.count} />
         <Title>South Park Click!</Title>
         <div className="container col-8">
           {this.state.friends.map(friend =>
             <FriendCard
+              // shuffle={this.shuffleCards}
+              southParkMemory={this.southParkMemory}
               key={friend.id}
               id={friend.id}
               name={friend.name}
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./style.css";
 
+
+
 function FriendCard(props) {
+    // function shuffleCards(arr) {
+    //     for (let i = arr.length - 1; i > 0; i--) {
+    //       const j = Math.floor(Math.random() * arr.length)
+    //       const temp = arr[i]
+    //       arr[i] = arr[j]
+    //       arr[j] = temp
+    //     }
+    //     return arr
+    //   };
+    // const shuffle = shuffleCards(props);
+
     return (
         // <div className="container">
+        <span onClick={() => props.southParkMemory(props.id)} className="memory">
             <button type="button" className="btn">
                 <div className="card">
                     <div className="card-wrapper">
@@ -17,6 +31,7 @@ function FriendCard(props) {
                     </div>
                 </div>
             </button>
+        </span>
         // </div>
     );
 };
