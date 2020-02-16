4dd4862b244ea445d84b27a4698c5635d25e8b0e
aancjr <andrewancjr@gmail.com>
2020-02-16 08:54:26 -0500

    adjusted background image, updated button class to have no outline, added media query for large desktop resolutions, some margin updates.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import FriendCard from "./components/FriendCard"
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
 import Navbar from "./components/Navbar";
 import friends from "./friends.json";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
-
   state = {
     clicked: [],
     friends,
@@ -18,53 +15,44 @@ class App extends Component {
     count: 0
   };
 
-
   southParkMemory = id => {
-
     if (this.state.clicked.includes(id)) {
-
-      alert("Try again!");
+      alert("Okay, let's try this again!");
 
       this.setState({
         clicked: [],
         friends,
-        // highScore: this.highscore(),
         count: 0
       });
 
-      console.log(`High Score: ${this.state.highScore}`)
-
+      console.log(`High Score: ${this.state.highScore}`);
     } else {
-
       this.setState(prevState => ({
         clicked: [id, ...prevState.clicked],
         count: this.state.count + 1
       }));
 
       console.log(this.state.clicked);
-
     }
   };
 
   highscore = () => {
     if (this.state.count > this.state.highScore) {
-      this.setState({ highScore: this.state.count })
+      this.setState({ highScore: this.state.count });
     }
-  }
+  };
 
   shuffleCards = arr => {
-
     if (this.state.count <= 12) {
       for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * arr.length)
-        const temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-      };
+        const j = Math.floor(Math.random() * arr.length);
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
 
-    };
-
-    return arr
+    return arr;
   };
 
   componentDidMount() {
@@ -74,36 +62,33 @@ class App extends Component {
   componentDidUpdate() {
     this.shuffleCards(friends);
     this.highscore();
-
   }
 
-
   render() {
-
     return (
       <>
-        <Navbar className="" count={this.state.count} highscore={this.state.highScore} />
+        <Navbar
+          count={this.state.count}
+          highscore={this.state.highScore}
+        />
 
-        <Container className="">
-          <Row className="mt-3 mx-1 tile-row justify-content-center">
-            {this.state.friends.map(friend =>
-              <Card className="">
-                <FriendCard
-                  southParkMemory={this.southParkMemory}
-                  key={friend.id}
-                  id={friend.id}
-                  name={friend.name}
-                  image={friend.image}
-                  alias={friend.alias}
-                />
-              </Card>
-            )}
+        <Container>
+          <Row className="tile-row justify-content-center">
+            {this.state.friends.map(friend => (
+              <FriendCard
+                southParkMemory={this.southParkMemory}
+                key={friend.id}
+                id={friend.id}
+                name={friend.name}
+                image={friend.image}
+                alias={friend.alias}
+              />
+            ))}
           </Row>
         </Container>
-
       </>
-    )
-  };
+    );
+  }
 }
 
 export default App;
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,17 +4,29 @@ import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
 import "./style.css";
 
-
 function FriendCard(props) {
-    return (
-        <Button className="btn" variant="outline-light" onClick={() => props.southParkMemory(props.id)}>
-            <Image className="imgsize " varient="top" src={props.image} alt={props.name} />
-            <Card.Body className="cardbody">
-                <Card.Text className="title text-center">{props.name}</Card.Text>
-                <Card.Text className="cardtext text-center"><strong>Alias:</strong> {props.alias}</Card.Text>
-            </Card.Body>
-        </Button>
-    );
-};
+  return (
+    <Card>
+      <Button
+        className="btn"
+        variant="outline-*"
+        onClick={() => props.southParkMemory(props.id)}
+      >
+        <Image
+          className="imgsize"
+          varient="top"
+          src={props.image}
+          alt={props.name}
+        />
+        <Card.Body className="cardbody">
+          <Card.Text className="title text-center">{props.name}</Card.Text>
+          <Card.Text className="cardtext text-center">
+            <strong>Alias:</strong> {props.alias}
+          </Card.Text>
+        </Card.Body>
+      </Button>
+    </Card>
+  );
+}
 
 export default FriendCard;
