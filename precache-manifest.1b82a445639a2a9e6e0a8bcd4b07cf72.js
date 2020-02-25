37cd4e79af9c3ca7c31a10c27025241469d4888d
aancjr <andrewancjr@gmail.com>
2020-02-24 20:12:38 -0500

    Updates

---
+++ precache-manifest.1b82a445639a2a9e6e0a8bcd4b07cf72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c86475120bf712de98020f8b1d082e",
+    "url": "/south-park-click/index.html"
+  },
+  {
+    "revision": "918c41500a3d1f69f675",
+    "url": "/south-park-click/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cf71819dd13d890edd3b",
+    "url": "/south-park-click/static/css/main.9a283a5f.chunk.css"
+  },
+  {
+    "revision": "918c41500a3d1f69f675",
+    "url": "/south-park-click/static/js/2.5fb22d8f.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/south-park-click/static/js/2.5fb22d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf71819dd13d890edd3b",
+    "url": "/south-park-click/static/js/main.c742cbe6.chunk.js"
+  },
+  {
+    "revision": "592838d79fcab7958b79",
+    "url": "/south-park-click/static/js/runtime-main.f452d14d.js"
+  },
+  {
+    "revision": "38cebca078de107f2e361471adfdb67d",
+    "url": "/south-park-click/static/media/background.38cebca0.jpg"
+  }
+]);
